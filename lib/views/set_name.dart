6da3020ca82fdf5e11055f9f141f468b500dffe2
saum7800 <gandhisaumya8@gmail.com>
2020-11-14 18:17:11 +0530

    restyled 1st page

--- lib/views/set_name.dart
+++ lib/views/set_name.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meme_battle/synced_models/Game.dart';
 import 'package:provider/provider.dart';
+import 'package:nice_button/nice_button.dart';
 
 class SetNameView extends StatefulWidget {
   @override
@@ -10,29 +11,72 @@ class SetNameView extends StatefulWidget {
 
 class _SetNameViewState extends State<SetNameView> {
   TextEditingController nameCtrl = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     Game game = Provider.of<Game>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MemeBattle"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(hintText: "Enter Your Nickname!"),
-              controller: nameCtrl,
-            ),
-            FloatingActionButton.extended(
-                onPressed: () {
-                  game.setName(nameCtrl.text);
-                },
-                label: Text("Confirm Name!"))
-          ],
-        ),
-      ),
-    );
+
+    Color hexToColor(String code) {
+      return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+    }
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Meme Battle",
+        home: new Material(
+            child: new Container(
+                padding: const EdgeInsets.all(30.0),
+                color: Colors.white,
+                child: new Container(
+                  child: new Center(
+                      child: new Column(children: [
+                    new Padding(padding: EdgeInsets.only(top: 140.0)),
+                    new Text(
+                      'Meme Battle',
+                      style: new TextStyle(
+                          color: hexToColor("#F2A03D"), fontSize: 25.0),
+                    ),
+                    new Padding(padding: EdgeInsets.only(top: 50.0)),
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: new InputDecoration(
+                                labelText: "Nickname",
+                                fillColor: Colors.white,
+                                border: new OutlineInputBorder(
+                                  borderRadius: new BorderRadius.circular(25.0),
+                                  borderSide: new BorderSide(),
+                                ),
+                                //fillColor: Colors.green
+                              ),
+                              controller: nameCtrl,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter a nickname';
+                                }
+                                return null;
+                              },
+                              style: new TextStyle(fontFamily: "Poppins"),
+                            ),
+                            new Padding(padding: EdgeInsets.only(top: 50.0)),
+                            NiceButton(
+                              // width: 515,
+                              elevation: 8.0,
+                              radius: 52.0,
+                              text: "Enter",
+                              background: Color(0xff5b86e5),
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  game.setName(nameCtrl.text);
+                                }
+                              },
+                            ),
+                          ],
+                        )),
+                  ])),
+                ))));
   }
 }
